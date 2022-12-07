35480c0cca78c50844a3d05bcb2e06bcaa9c90a5
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-07 16:17:20 +0530

    search course

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -15,4 +15,8 @@ export class ApiService {
   {
     return this.http.post("http://localhost:8080/add",dataToSend)
   }
+  searchCourse=(dataToSend:any)=>
+  {
+    return this.http.post("http://localhost:8080/search",dataToSend)
+  }
 }
---
+++ src/app/search-course/search-course.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-search-course',
+  templateUrl: './search-course.component.html',
+  styleUrls: ['./search-course.component.css']
+})
+export class SearchCourseComponent {
+  constructor(private api:ApiService){}
+  courseTitle=""
+
+  searchdata:any=[]
+  readValues=()=>
+
+  {
+
+    let data:any={
+
+      "courseTitle":this.courseTitle
+    }
+
+    console.log(data)
+
+    this.api.searchCourse(data).subscribe(
+
+      (response:any)=>{
+
+        console.log(response)
+
+        if (response.length==0) {
+
+          alert("invalide employee code")
+
+         
+
+        } else {
+
+          this.searchdata = response
+
+        }
+
+      }
+
+    )
+
+  }
+}
