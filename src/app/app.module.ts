2bbfee708282e1708e17e67e8f99aa94f4dee9eb
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-07 13:31:21 +0530

    development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { ViewCourseComponent } from './view-course/view-course.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const myRoute:Routes=[
   {
@@ -31,6 +31,7 @@ const myRoute:Routes=[
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    RouterModule.forRoot(myRoute),
     HttpClientModule
   ],
   providers: [],
